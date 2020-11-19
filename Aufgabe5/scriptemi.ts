a815f91d57a8c00bbf5e4a5e338a65546a6a322f
c-kocheise <christoph.kocheise@hs-furtwangen.de>
2020-11-19 11:44:13 +0100

    Complete Relationen

--- Aufgabe5/scriptemi.ts
+++ Aufgabe5/scriptemi.ts
@@ -36,20 +36,28 @@ const relAfrica: number = (africa2018 / weltweit2018 * 100).toFixed(1);
 const zuAfricaP: number = (100 - (africa2008 / africa2018 *100)).toFixed(1);
 const zuAfricaZ: number = (africa2018 - africa2008).toFixed(1);
 
-//relsAmerika
-const relSAmerika: number = (sAmerica2018 / weltweit2018 * 100).toFixed(1);
+//relSAmerika
+const relSAmerica: number = (sAmerica2018 / weltweit2018 * 100).toFixed(1);
+const zuSAmericaP: number = (100 - (sAmerica2008 / sAmerica2018 *100)).toFixed(1);
+const zuSAmericaZ: number = (sAmerica2018 - sAmerica2008).toFixed(1);
 
 //relEuropa
 const relEurope: number = (europe2018 / weltweit2018 *100).toFixed(1);
+const zuEuropeP: number = (100 - (europe2008 / europe2018 *100)).toFixed(1);
+const zuEuropeZ: number = (europe2018 - europe2008).toFixed(1);
 
-//relnAmerika
+//relNAmerika
 const relNAmerika: number = (nAmerica2018 / weltweit2018 *100).toFixed(1);
+const zuNAmericaP: number = (100 - (nAmerica2008 / nAmerica2018 *100)).toFixed(1);
+const zuNAmericaZ: number = (nAmerica2018 - nAmerica2008).toFixed(1);
 
 //relAsien
 const relAsia: number = (asia2018 / weltweit2018 *100).toFixed(1);.toFixed(1)
+const zuAsiaP: number = (100 - (asia2008 / asia2018 *100)).toFixed(1);
+const zuAsiaZ: number = (asia2018 - asia2008).toFixed(1);
 
 //relAustralien
 const relAustralia: number = (australia2018 / weltweit2018 *100).toFixed(1);
+const zuAustraliaP: number = (100 - (australia2008 / australia2018 *100)).toFixed(1);
+const zuAustraliaZ: number = (australia2018 - australia2008).toFixed(1);
 
-
-//Relationen der Jahre
