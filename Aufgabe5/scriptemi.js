e21741adc741d9082564bfbea1f92b44ab8b5ab3
c-kocheise <christoph.kocheise@hs-furtwangen.de>
2020-11-19 13:45:11 +0100

    Create Javascript dokuments

---
+++ Aufgabe5/scriptemi.js
@@ -0,0 +1,54 @@
+//Emissionen der Länder
+//Afrika
+var africa2008 = 1028;
+var africa2018 = 1235;
+//Süd Amerika
+var sAmerica2008 = 1132;
+var sAmerica2018 = 1261;
+//Europa
+var europe2008 = 4965;
+var europe2018 = 4209;
+//Nord Amerika
+var nAmerica2008 = 6600;
+var nAmerica2018 = 6035;
+//Asien
+var asia2008 = 12954;
+var asia2018 = 16274;
+//Australien
+var australia2008 = 1993;
+var australia2018 = 2100;
+//Berechnungen
+//Emissionen Weltweit
+var weltweit2018 = africa2018 + sAmerica2018 + europe2018 + nAmerica2018 + asia2018 + australia2018;
+//Relationen
+//relAfrika
+var relAfrica = (africa2018 / weltweit2018 * 100).toFixed(1);
+var zuAfricaP = (100 - (africa2008 / africa2018 * 100)).toFixed(1);
+var zuAfricaZ = (africa2018 - africa2008).toFixed(1);
+//relSAmerika
+var relSAmerica = (sAmerica2018 / weltweit2018 * 100).toFixed(1);
+var zuSAmericaP = (100 - (sAmerica2008 / sAmerica2018 * 100)).toFixed(1);
+var zuSAmericaZ = (sAmerica2018 - sAmerica2008).toFixed(1);
+//relEuropa
+var relEurope = (europe2018 / weltweit2018 * 100).toFixed(1);
+var zuEuropeP = (100 - (europe2008 / europe2018 * 100)).toFixed(1);
+var zuEuropeZ = (europe2018 - europe2008).toFixed(1);
+//relNAmerika
+var relNAmerika = (nAmerica2018 / weltweit2018 * 100).toFixed(1);
+var zuNAmericaP = (100 - (nAmerica2008 / nAmerica2018 * 100)).toFixed(1);
+var zuNAmericaZ = (nAmerica2018 - nAmerica2008).toFixed(1);
+//relAsien
+var relAsia = (asia2018 / weltweit2018 * 100).toFixed(1);
+var zuAsiaP = (100 - (asia2008 / asia2018 * 100)).toFixed(1);
+var zuAsiaZ = (asia2018 - asia2008).toFixed(1);
+//relAustralien
+var relAustralia = (australia2018 / weltweit2018 * 100).toFixed(1);
+var zuAustraliaP = (100 - (australia2008 / australia2018 * 100)).toFixed(1);
+var zuAustraliaZ = (australia2018 - australia2008).toFixed(1);
+//Console
+console.log('Emissionswerte Afrika');
+console.log('Die Emission von Arfika in 2018 ist: ' + );
+var africa2018;
++'kg';
+;
+//# sourceMappingURL=scriptemi.js.map
