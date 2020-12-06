4f1d5671706a9db0ae36d0b68cce998ce33ce5f1
c-kocheise <christoph.kocheise@hs-furtwangen.de>
2020-12-06 16:31:34 +0100

    Add audio drumpad .ts

---
+++ Aufgabe7/drum.ts
@@ -0,0 +1,11 @@
+
+    let drumPad = [];
+    drumPad[0] = new Audio('drummedia/A.mp3');
+    drumPad[1] = new Audio('drummedia/C.mp3');
+    drumPad[2] = new Audio('drummedia/F.mp3');
+    drumPad[3] = new Audio('drummedia/G.mp3');
+    drumPad[4] = new Audio('drummedia/hihat.mp3');
+    drumPad[5] = new Audio('drummedia/kick.mp3');
+    drumPad[6] = new Audio('drummedia/laugh-1.mp3');
+    drumPad[7] = new Audio('drummedia/laugh-2.mp3');
+    drumPad[8] = new Audio('drummedia/snare.mp3');
