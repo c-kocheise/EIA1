3987e05e01b4776d964a42364bab0d73fc5e2f2c
c-kocheise <christoph.kocheise@hs-furtwangen.de>
2020-12-06 16:35:45 +0100

    Add eventlistener

--- Aufgabe7/drum.ts
+++ Aufgabe7/drum.ts
@@ -1,3 +1,4 @@
+//Drumpad Samples
 
     let drumPad = [];
     drumPad[0] = new Audio('drummedia/A.mp3');
@@ -9,3 +10,42 @@
     drumPad[6] = new Audio('drummedia/laugh-1.mp3');
     drumPad[7] = new Audio('drummedia/laugh-2.mp3');
     drumPad[8] = new Audio('drummedia/snare.mp3');
+
+
+//Eventlistener
+
+    document.querySelector(".sound1").addEventListener("click", function(){
+        playSample(0);
+        });
+
+    document.querySelector(".sound2").addEventListener("click", function(){
+        playSample(1);
+        });
+
+    document.querySelector(".sound3").addEventListener("click", function(){
+        playSample(2);
+        });
+        
+    document.querySelector(".sound4").addEventListener("click", function(){
+        playSample(3);
+        });
+
+    document.querySelector(".sound5").addEventListener("click", function(){
+        playSample(4);
+        });
+
+    document.querySelector(".sound6").addEventListener("click", function(){
+        playSample(5);
+        });
+
+    document.querySelector(".sound7").addEventListener("click", function(){
+        playSample(6);
+        });
+
+    document.querySelector(".sound8").addEventListener("click", function(){
+        playSample(7);
+        });
+
+    document.querySelector(".sound9").addEventListener("click", function(){
+        playSample(8);
+        });
