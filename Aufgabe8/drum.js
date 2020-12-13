10548a2ec2078c52249b4c8173373bb2f47f0b7f
c-kocheise <christoph.kocheise@hs-furtwangen.de>
2020-12-13 12:55:15 +0100

    Add Buttons Css

--- Aufgabe8/drum.js
+++ Aufgabe8/drum.js
@@ -12,47 +12,91 @@ drumPad[8] = new Audio('drummedia/snare.mp3');
 //Eventlistener
 document.querySelector(".sound1").addEventListener("click", function () {
     playSample(0);
+    recbeat(0);
 });
 document.querySelector(".sound2").addEventListener("click", function () {
     playSample(1);
+    recbeat(1);
 });
 document.querySelector(".sound3").addEventListener("click", function () {
     playSample(2);
+    recbeat(2);
 });
 document.querySelector(".sound4").addEventListener("click", function () {
     playSample(3);
+    recbeat(3);
 });
 document.querySelector(".sound5").addEventListener("click", function () {
     playSample(4);
+    recbeat(4);
 });
 document.querySelector(".sound6").addEventListener("click", function () {
     playSample(5);
+    recbeat(5);
 });
 document.querySelector(".sound7").addEventListener("click", function () {
     playSample(6);
+    recbeat(6);
 });
 document.querySelector(".sound8").addEventListener("click", function () {
     playSample(7);
+    recbeat(7);
 });
 document.querySelector(".sound9").addEventListener("click", function () {
     playSample(8);
+    recbeat(8);
 });
 //Funktion für DrumPads
 function playSample(i) {
     drumPad[i].play();
 }
 ;
-//Eventlistener PlayButton
-document.querySelector(".playButton").addEventListener("click", function () {
-    playButton();
+//Eventlistener PlayButton, PauseButton
+playButton.addEventListener("click", function () {
+    playSchleife(true);
+    playButton.classList.add("inactive");
+    pauseButton.classList.remove("inactive");
 });
-//Funktion Playbutton
-function playButton() {
-    setInterval(function () {
-        drumPad[8].play();
-        drumPad[4].play();
-        drumPad[5].play();
-    }, 500);
+pauseButton.addEventListener("click", function () {
+    playSchleife(false);
+    pauseButton.classList.add("inactive");
+    playButton.classList.remove("inactive");
+});
+//Eventlistener Aufnehmen und Löschen
+recbutton.addEventListener("click", function () {
+    if (recbutton.classList.contains("active")) {
+        recbutton.classList.remove("active");
+        abfrage = false;
+    }
+    else {
+        recbutton.classList.add("active");
+        abfrage = true;
+    }
+});
+trash.addEventListener("click", function () {
+    beat.splice(0, beat.length);
+});
+//Aufnahme
+function recbeat(i) {
+    if (abfrage == true) {
+        beat.push(i);
+    }
 }
-;
+//Funktion PlayButton, PauseButton
+function playSchleife(b) {
+    if (b == true) {
+        myInterval = setInterval(function () {
+            if (x < beat.length) {
+                playSample(beat[x]);
+                x++;
+            }
+            else {
+                x = 0;
+            }
+        }, 500);
+    }
+    else {
+        clearInterval(myInterval);
+    }
+}
 //# sourceMappingURL=drum.js.map
