42de4df8e391dd39fc48b25ef2f44ca552f0997b
c-kocheise <christoph.kocheise@hs-furtwangen.de>
2020-12-13 14:30:37 +0100

    Fix Schönheitsfehler Anführungszeichen

--- Aufgabe8/drum.js
+++ Aufgabe8/drum.js
@@ -1,14 +1,14 @@
 //Drumpad Samples
 var drumPad = [];
-drumPad[0] = new Audio('drummedia/A.mp3');
-drumPad[1] = new Audio('drummedia/C.mp3');
-drumPad[2] = new Audio('drummedia/F.mp3');
-drumPad[3] = new Audio('drummedia/G.mp3');
-drumPad[4] = new Audio('drummedia/hihat.mp3');
-drumPad[5] = new Audio('drummedia/kick.mp3');
-drumPad[6] = new Audio('drummedia/laugh-1.mp3');
-drumPad[7] = new Audio('drummedia/laugh-2.mp3');
-drumPad[8] = new Audio('drummedia/snare.mp3');
+drumPad[0] = new Audio("drummedia/A.mp3");
+drumPad[1] = new Audio("drummedia/C.mp3");
+drumPad[2] = new Audio("drummedia/F.mp3");
+drumPad[3] = new Audio("drummedia/G.mp3");
+drumPad[4] = new Audio("drummedia/hihat.mp3");
+drumPad[5] = new Audio("drummedia/kick.mp3");
+drumPad[6] = new Audio("drummedia/laugh-1.mp3");
+drumPad[7] = new Audio("drummedia/laugh-2.mp3");
+drumPad[8] = new Audio("drummedia/snare.mp3");
 //Eventlistener
 document.querySelector(".sound1").addEventListener("click", function () {
     playSample(0);
