2f85d5c785a31eccbf666cf427c53c3c49627b74
c-kocheise <christoph.kocheise@hs-furtwangen.de>
2020-12-20 13:27:17 +0100

    Elemente und Kinder

--- Aufgabe9/toDo.ts
+++ Aufgabe9/toDo.ts
@@ -12,8 +12,28 @@ let index = 0;
 function addToHTML() {
     let list = document.getElementById("Task");
     let currentElement = document.getElementById("myInput").value;
+    
     // Neues Element
     let newElement = document.createElement("li");
     let inputText = document.createElement("label");
     let newCheckbox = document.createElement("input");
     let trashIcon = document.createElement("i");
+
+     // Elemente gleichsetzen
+     inputText.innerHTML = currentElement;
+     newElement.className = "Task";
+     trashIcon.className = "fas fa-trash-alt trash";
+     newCheckbox.type = "checkbox";
+     newCheckbox.className = "checkbox";
+
+     // Kinder
+     list.appendChild(newElement);
+     newElement.appendChild(newCheckbox);
+     newElement.appendChild(inputText);
+     newElement.appendChild(trashIcon);
+     trashIcon.addEventListener("click", function () {
+         newElement.remove();
+         index--;
+         counter();
+     });
+ }
